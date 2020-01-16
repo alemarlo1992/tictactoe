0d2ae7e0167b7313b71567ff69cbf94eaee060af
Alejandra Lopez <alemarlo1992@gmail.com>
2020-01-16 14:48:36 -0800

    Built a class for board, welcome message, and refresh function

---
+++ tictactoe.py
@@ -0,0 +1,38 @@
+""" Tic Tac Toe game using OOP"""
+import os 
+
+class Board(object):
+    """ Board for tic tac toe """
+    def __init__(self):
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    def display(self):
+        print(f"{self.cells[1]} | {self.cells[2]} | {self.cells[3]}")
+        print("__________")
+        print(f"{self.cells[4]} | {self.cells[5]} | {self.cells[6]}")
+        print("__________")
+        print(f"{self.cells[7]} | {self.cells[8]} | {self.cells[9]}")
+
+    def update_cell(self, cell_num, player):
+        """ Update cell after user input """
+        self.cells[cell_num] = player 
+
+board = Board()
+
+def welcome_message():
+    """ Welcome message for players """
+    print("Hello! Welcome to Tic Tac Toe")
+
+def refresh_screen():
+    """ Refresh screen when there has been an update to board """ 
+
+    # clear screen 
+    os.system("clear")
+
+    # print welcome message 
+    welcome_message()
+
+    #display board to player 
+    board.display()
+
+
